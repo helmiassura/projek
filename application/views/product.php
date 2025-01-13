cd16104355e3309cb08a9607bdb75bc04b1cb87f
helmiassura <lerkud600@gmail.com>
2025-01-13 20:13:26 +0700

    update navbar

--- application/views/product.php
+++ application/views/product.php
@@ -96,8 +96,7 @@
       <!-- Sidebar Menu -->
       <nav class="mt-2">
         <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
-          <!-- Add icons to the links using the .nav-icon class
-               with font-awesome or any other icon font library -->
+        <li class="nav-header" style="color: grey;">MAIN NAVIGATION</li>
           <li class="nav-item menu-open">
             <a href="<?=site_url('dashboard')?>" class="nav-link">
               <i class="nav-icon fas fa-tachometer-alt"></i>
